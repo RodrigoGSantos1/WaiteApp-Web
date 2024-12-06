ca655a25a04906e3e5cb2fd73e1471015d5aed5f
Rodrigo Guedes <rodrigo.guantos@gmail.com>
2024-12-06 18:47:14 -0300

    create home page and StatusCard

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,16 @@
 import { StatusCard } from '@/components/UI'
 import HomeIcon from '@/assets/icons/interface/home.svg'
 import RefreshIcon from '@/assets/icons/interface/refresh.svg'
+import Image from 'next/image'
 
 import ClockImage from '@/assets/icons/category/clock.png'
-import Image from 'next/image'
+import ChefImage from '@/assets/icons/category/cozinhando.png'
+import CheckImange from '@/assets/icons/category/check.png'
 
 export default function Home() {
   const Clock = () => <Image src={ClockImage} alt="relogio" width={30} />
+  const Chef = () => <Image src={ChefImage} alt="chef" width={30} />
+  const Check = () => <Image src={CheckImange} alt="check" width={30} />
 
   return (
     <div className="w-full h-full flex flex-col">
@@ -27,8 +31,8 @@ export default function Home() {
       </div>
       <div className="grid grid-cols-3 gap-6 w-full mt-20">
         <StatusCard icon={<Clock />} title="Fila de espera" />
-        <StatusCard icon={<Clock />} title="Fila de espera" />
-        <StatusCard icon={<Clock />} title="Fila de espera" />
+        <StatusCard icon={<Chef />} title="Em Produção" />
+        <StatusCard icon={<Check />} title="Pronto" />
       </div>
     </div>
   )
--- src/components/UI/StatusCard.tsx
+++ src/components/UI/StatusCard.tsx
@@ -2,13 +2,16 @@ import { IStatusCard } from '../@types/StatusCard'
 
 export function StatusCard({ icon, title }: IStatusCard) {
   return (
-    <div className="rounded-xl border border-gray-300/40 flex flex-col items-center justify-center">
-      <div className="flex">
+    <div className="rounded-md border border-gray-300/40 bg-white-200 flex flex-col items-center justify-center p-4">
+      <div className="flex items-center mb-8">
         {icon}
-        <h5 className="text-h5">{title}</h5>
-        <div className="bg-gray-300/20">1</div>
+        <h5 className="text-h5 mx-5">{title}</h5>
+        <div className="bg-gray-300/20 py-1 px-2 rounded">1</div>
       </div>
-      <div>conteudo</div>
+      <div className="bg-white-100 w-full h-[128px] rounded-md border border-gray-300/40 flex flex-col items-center justify-center">
+        <span className="text-medium">Messa 2</span>
+        <p className="text-small text-gray-700">2 items</p>
+      </div>
     </div>
   )
 }
